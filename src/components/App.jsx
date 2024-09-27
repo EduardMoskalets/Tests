958889ef52d484972beaf9bc425294e45bf56bd8
Eduard <edik.moskalets@gmail.com>
2024-09-27 13:38:38 +0300

    setting

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+
+import css from './App.module.css';
+import Header from './Header/Header';
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+
+export default function Header() {
+    return (
+<h1>sds</h1>
+    );
+}
